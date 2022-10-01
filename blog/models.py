fd9a6850f0f286e276c31d1b2c7c5047171cb706
Julius-Okeyo <jaykopiyo@gmail.com>
2022-10-01 12:46:17 +0300

    Created Blog_app to learn django

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    author = models.ForeignKey(
+        "auth.user", on_delete = models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse ("post_detail",kwarg = {"pk":self.pk})
